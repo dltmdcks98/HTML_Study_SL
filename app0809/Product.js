ecf938f5b282c2354b358c3e41a6c06395aa1065
dltmdcks98 <dltmdcks98@gmail.com>
2022-08-09 11:54:19 +0900

    22.08.09쇼핑몰 배너

--- app0809/Product.js
+++ app0809/Product.js
@@ -18,11 +18,30 @@ class Product{
 
         //생성된 DOM객체를 원하는 부모요소에 부착
         this.container.appendChild(this.img);
+
+        //이미지를 대상으로 마우스 이벤트 연결
+        this.img.addEventListener("mouseover",function(){
+            //현재 돌아가는 루프를 정지
+            flag=false;
+        });
+        this.img.addEventListener("mouseout",function(){
+            //현재 돌아가는 루프를 다시 재생
+            flag=true;
+        });
+
+        //클릭하면 해당 이미지를 새로운 탭으로 보여주기 
+        this.img.addEventListener("click",function(){
+           window.open(this.src,"_blank");//_blank : 새로운 창으로 열기
+        });
+        
     }
 
     //물체 이동 메소드,생성된 이후에도 left값을 변경하여 좌표에 적용할 것이므로
     move(){
         this.img.style.left=this.x+"px";
+        if(this.x< -(this.width+5)){
+            this.x = (this.width+5)*6;
+        }
     }
 
 }
